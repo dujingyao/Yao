ed4109ec7e7e56877bf866119a937b99cf9a859b
Yao <1109228371@qq.com>
2024-11-04 10:21:30 +0800

    11.4

---
+++ acwing/prime生成最小树.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+using namespace std;
+const int N=510;
+int Grap[N][N];  //邻接矩阵
+int dist[N];  //标记最短距离
+int st[N];   //标记是否走过
+int pre[N];  //记录前父节点
+void prime(){
+    
+}
+
+int main(){
+    
+    return 0;
+}
