30b1aa48470d1d602317e968fd3011cb3e014f75
Yao <1109228371@qq.com>
2024-11-06 18:49:25 +0800

    11.6

--- acwing/二分图的最大匹配.cpp
+++ acwing/二分图的最大匹配.cpp
@@ -1,7 +1,47 @@
 #include<iostream>
+#include<algorithm>
+#include<cstring>
 using namespace std;
+const int N=510;
+const int M=1e5+10;
+int h[N],e[M],ne[M],idx;
+int match[N];
+bool st[N];
+int n1,n2,m;
+
+void add(int a,int b){
+    e[idx]=b;
+    ne[idx]=h[a];
+    h[a]=idx++;
+}
+
+bool find(int x){
+    for(int i=h[x];i!=-1;i=ne[i]){
+        int j=e[i];
+        if(!st[j]){
+            st[j]=true;
+            if(match[j]==0||find(match[j])){
+                match[j]=x;
+                return true;
+            }
+        }
+    }
+    return false;
+}
 
 int main(){
-    
+    cin>>n1>>n2>>m;
+    memset(h,-1,sizeof(h));
+    while(m--){
+        int a,b;
+        cin>>a>>b;
+        add(a,b);
+    }
+    int res=0;
+    for(int i=1;i<=n1;i++){
+        memset(st,false,sizeof(st));
+        if(find(i)) res++;
+    }
+    cout<<res<<endl;
     return 0;
 }
