1d1af3a2c81448caf56c3ed55ab753c5ea8174e1
dujingyao <1108228371@qq.com>
2024-11-15 16:04:54 +0800

    11.14

--- acwing/筛法求欧拉函数.cpp
+++ acwing/筛法求欧拉函数.cpp
@@ -5,7 +5,7 @@ const int N=1e6+10;
 
 bool st[N];  //记录是否为合数
 int primes[N],dex;   //素数表
-int euler[N];
+int euler[N];//欧拉函数
 
 void get_euler(int x){
     euler[1]=1;
@@ -17,7 +17,7 @@ void get_euler(int x){
         for(int j=0;primes[j]<=x/i;j++){
             int t=primes[j]*i;  //t是一个合数
             st[t]=true;//登记t为合数
-            if(i%primes[j]==0){
+            if(i%primes[j]==0){//i为primes[j]的一个约数
                 euler[t]=euler[i]*primes[j];
                 break;
             }
@@ -29,10 +29,7 @@ void get_euler(int x){
 int main(){
     int n;
     cin>>n;
-    while(n--){
-        int x;
-        cin>>x;
-  
-    }
+    get_euler(n);
+    
     return 0;
 }
