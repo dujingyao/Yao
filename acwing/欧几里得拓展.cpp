1b3322844246f24fedb2c6242e66a263e153538d
Yao <1109228371@qq.com>
2024-11-20 23:06:17 +0800

    11.20

---
+++ acwing/欧几里得拓展.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int exgcd(int a,int b,int &x,int &y){
+    if(!b){
+        x=1,y=0;
+        return a;
+    }
+    int d=exgcd(b,a%b,y,x);
+    y-=a/b*x;
+    return d;
+}
+int main(){
+    int n;
+    cin>>n;
+    while(n--){
+        int a,b;
+        cin>>a>>b;
+        int x,y;
+        exgcd(a,b,x,y);
+        cout<<x<<' '<<y<<endl;
+    }
+    return 0;
+}
