0b4e58775fe323a8e94d49c64fdd47b4ad2399cd
Yao <1109228371@qq.com>
2025-03-21 16:54:30 +0800

    一些python的配置

--- 9.28/1.py
+++
@@ -1,8 +0,0 @@
-def main():
-    print("Hello, Python!")
-    x = 10
-    y = 20
-    print(f"x + y = {x + y}")
-
-if __name__ == "__main__":
-    main()
--- test.py
+++
@@ -1,8 +0,0 @@
-def main():
-    print("虚拟环境测试")
-    x = 10
-    y = 20
-    print(f"x + y = {x + y}")
-
-if __name__ == "__main__":
-    main()
